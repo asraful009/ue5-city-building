6f89a76f4f7047e73cb1bec335eceadc34115f3f
Asraful Islam <asraful009@gmail.com>
2026-02-25 23:13:48 +0600

    b

---
+++ Source/ZMR/Public/Building/ZMR_ResidentialBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Building/ZMR_BuildingBase.h"
+#include "ZMR_ResidentialBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZMR_API AZMR_ResidentialBase : public AZMR_BuildingBase
+{
+  GENERATED_BODY()
+public:
+  AZMR_ResidentialBase();
+};
